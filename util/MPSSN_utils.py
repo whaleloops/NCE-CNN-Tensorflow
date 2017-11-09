8cd21ffc1ac09fde744a3139d76ed6152cfc85e9
Dongxu Zhang <dongxuzhang@gpu-0-5.blake>
2017-11-09 13:33:47 -0500

    add 1e-6 inside sqrt

--- util/MPSSN_utils.py
+++ util/MPSSN_utils.py
@@ -12,18 +12,19 @@ def compute_l1_distance(x, y):
 
 def compute_euclidean_distance(x, y):
     with tf.name_scope('euclidean_distance'):
-        d = tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(x, y)), axis=1))
+        d = tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(x, y)), axis=1) + 1e-6)
         return d
 
 def compute_cosine_distance(x, y):
     with tf.name_scope('cosine_distance'):
-        x_norm = tf.sqrt(tf.reduce_sum(tf.square(x), axis=1))
-        y_norm = tf.sqrt(tf.reduce_sum(tf.square(y), axis=1))
+        x_norm = tf.sqrt(tf.reduce_sum(tf.square(x), axis=1) + 1e-6)
+        y_norm = tf.sqrt(tf.reduce_sum(tf.square(y), axis=1) + 1e-6)
         x_y = tf.reduce_sum(tf.multiply(x, y), axis=1)
         d = tf.divide(x_y, tf.multiply(x_norm, y_norm))
         return d
 
 def comU1(x, y):
+    #t1 = compute_euclidean_distance(x, y)
     t1 = compute_cosine_distance(x, y)
     t2 = compute_euclidean_distance(x, y)
     t3 = compute_element_l1_distance(x, y)
@@ -34,6 +35,7 @@ def comU1(x, y):
     return result
 
 def comU2(x, y):
+    #result = [compute_euclidean_distance(x, y), compute_euclidean_distance(x, y)]
     result = [compute_cosine_distance(x, y), compute_euclidean_distance(x, y)]
     result = tf.stack(result, axis=1)
     # print 'comu2' #TODO
