6d4c6392693942fd9fea2f2d503fc37e74c90d6e
Dongxu Zhang <zhangdongxuu@gmail.com>
2017-11-15 11:20:11 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python main.py --lr=0.001 --reg=0.0001 --filterA=300 --filterB=20 --dim=150
+CUDA_VISIBLE_DEVICES=0 nohup python main.py --lr=0.0001 --reg=0.0001 --filterA=300 --filterB=20 --dim=150 --max_length=48 --batch_size=64 --update_tech=adam --keep_prob=0.5 &
